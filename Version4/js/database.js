30c92441fcf56d46db5a00abee926a3de9256817
Tinayu0915 <yutong029@126.com>
2017-04-23 16:13:49 +0800

    bugs modificated,work experience was added.Wish this really is finnal version

---
+++ Version4/js/database.js
@@ -0,0 +1,129 @@
+var Students=[
+    {
+    ID:1,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"三日",
+    branch:"南京"
+    },
+   {
+   ID:2,
+    category:"第二学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Ali",
+    branch:"常州"   
+   },
+   {
+   ID:3,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Baidu",
+    branch:"南京"   
+   },
+   {
+   ID:4,
+    category:"第一学期",
+    title:"PHP",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Tencent",
+    branch:"南京"   
+   },
+    {
+   ID:25,
+    category:"第二学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"果壳",
+    branch:"南京"   
+   },
+    {
+   ID:16,
+    category:"第一学期",
+    title:"PHP",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Summer",
+    branch:"南京"   
+   },
+    {
+   ID:27,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第144期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Tyler",
+    branch:"南京"   
+   },
+    {
+   ID:18,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第146期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Echo",
+    branch:"南京"   
+   },
+    {
+   ID:9,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-7-03",
+    creator:"Aamir",
+    branch:"常州"   
+   },
+    {
+   ID:10,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"技术部",
+    branch:"南京"   
+   },
+    {
+   ID:21,
+    category:"第一学期",
+    title:"HTML+CSS",
+    class:"欣才第141期",
+    status:"已发布",
+    time:"2016-11-03",
+    creator:"Tina",
+    branch:"南京"   
+   }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+]
---
+++ Version4/js/sorting_bigger.js
@@ -0,0 +1,12 @@
+function sorting_bigger(Students){
+      for(var k=0;k<Students.length;k++){
+      for(var l=k+1;l<Students.length;l++){
+        if(parseInt(Students[k].ID)>parseInt(Students[l].ID)){
+            container1=Students[k];
+            Students[k]=Students[l];
+            Students[l]=container1;
+
+        }
+      }
+      }  
+      }
