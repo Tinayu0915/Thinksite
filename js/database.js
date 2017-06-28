f5f179e8cea5acd52a23c1d92c587ae7a9f3ce16
yutong029@126.com <yutong029@126.com>
2017-06-29 02:11:56 +0800

    update index.html and css

--- js/database.js
+++ js/database.js
@@ -2,112 +2,102 @@ var Students=[
     {
     ID:1,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"三日",
-    branch:"南京"
+    creator:"教研部",
+    branch:"西安"
     },
    {
    ID:2,
     category:"第二学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Ali",
-    branch:"常州"   
+    creator:"教研部",
+    branch:"咸阳"   
    },
    {
    ID:3,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Baidu",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
    {
    ID:4,
     category:"第一学期",
-    title:"PHP",
-    class:"欣才第141期",
+    title:"晚自习",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Tencent",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
     {
    ID:25,
     category:"第二学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"果壳",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
     {
    ID:16,
     category:"第一学期",
-    title:"PHP",
-    class:"欣才第141期",
+    title:"晚自习",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Summer",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
     {
    ID:27,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第144期",
+    title:"数学+英语",
+    class:"鑫斯达第144期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Tyler",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
     {
    ID:18,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第146期",
+    title:"数学+英语",
+    class:"鑫斯达第146期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"Echo",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    },
     {
    ID:9,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-7-03",
-    creator:"Aamir",
-    branch:"常州"   
+    creator:"教研部",
+    branch:"咸阳"   
    },
     {
    ID:10,
     category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
+    title:"数学+英语",
+    class:"鑫斯达第141期",
     status:"已发布",
     time:"2016-11-03",
-    creator:"技术部",
-    branch:"南京"   
-   },
-    {
-   ID:21,
-    category:"第一学期",
-    title:"HTML+CSS",
-    class:"欣才第141期",
-    status:"已发布",
-    time:"2016-11-03",
-    creator:"Tina",
-    branch:"南京"   
+    creator:"教研部",
+    branch:"西安"   
    }
     
     
